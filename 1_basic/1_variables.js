f9a636dbd741099646720b7c7fb3a972c06faf35
Rajan Yadav <rajan.yadav2024@lpu.in>
2026-01-11 14:10:00 +0530

    variables in js

---
+++ 1_basic/1_variables.js
@@ -0,0 +1,20 @@
+const UID = 23456
+let emailId = "rajan@gmail.com"
+// var password = 123456
+city = "Gorakhpur"
+
+//const variable canno't be changed
+emailId = "shyam@gmail.com"
+//password = 98765
+
+
+if(true){
+    var password = 3456;
+}
+console.table([UID,emailId, password, city]);
+
+//don't use var && can't change const variable
+
+
+let me;
+console.log(me);
