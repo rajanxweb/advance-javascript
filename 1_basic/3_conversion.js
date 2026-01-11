9af8b7dc6b0c42cea43f6998160c0a273579d1d3
Rajan Yadav <rajan.yadav2024@lpu.in>
2026-01-11 14:10:58 +0530

    type conversion in js

---
+++ 1_basic/3_conversion.js
@@ -0,0 +1,9 @@
+let someNumber = 3245;
+let someString = String(someNumber);
+console.log(someString);
+
+let someBool = true;
+let someNum = Number(someBool);
+console.log(someNum)
+
+// NaN means not a number 
