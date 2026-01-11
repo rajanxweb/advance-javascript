59fdfaa2710da38624cae02ee46f3d48af1399ea
Rajan Yadav <rajan.yadav2024@lpu.in>
2026-01-11 14:10:17 +0530

    data type in js

---
+++ 1_basic/2_data_type.js
@@ -0,0 +1,12 @@
+/*
+string --name
+number -- 1,2
+bigint -- 12345678
+boolean -- true/false
+undefined -- undefined
+object -- null
+
+*/
+
+console.log(typeof null);
+console.log(typeof undefined);
